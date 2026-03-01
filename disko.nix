6f5103ba9acdd05716f010cdb8a722c2cce74fe9
tatpow <popovvadimka2008@gmail.com>
2026-03-01 17:03:47 +0300

    Add disko

---
+++ disko.nix
@@ -0,0 +1,40 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/nvme0n1"; # Rename to your disk name (lsblk)
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "partition";
+              size = "256M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            swap = {
+              size = "8G";
+              content = {
+                type = "swap";
+                resumeDevice = true;
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "btrfs";
+                mountpoint = "/";
+                extraArgs = [ "-L" "nixos" ];
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
--- flake.nix
+++ flake.nix
@@ -3,23 +3,28 @@
   description = "My own configuration";
 
   inputs = {
-
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, disko, ... }@inputs:
 
     let
       system = "x86_64-linux";
     in {
 
-      # Build configuration.nix
       nixosConfigurations.ZB-UX391F = nixpkgs.lib.nixosSystem {
 
         inherit system;
 
         modules = [
+          disko.nixosModules.disko
+          ./disko.nix
           ./nixos/configuration.nix
         ];
       };
