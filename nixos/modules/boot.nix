5920f9635edb87ecc4ba2cf8f81b5173e374d064
tatpow <popovvadimka2008@gmail.com>
2026-03-08 23:09:19 +0300

    Udpate problem with nowathdog

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -1,4 +1,12 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelParams = [ "nowatchdog" ];
+  boot.blacklistedKernelModules = [ "iTCO_wdt" "iTCO_vendor_support" ];
+
+  systemd.extraConfig = ''
+    DefaultTimeoutStopSec=10s
+    RuntimeWatchdogSec=0
+    ShutdownWatchdogSec=0
+  '';
 }
