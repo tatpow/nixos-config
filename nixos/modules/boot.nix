38837311e56604ed22507293d8755d4f950e7561
tatpow <popovvadimka2008@gmail.com>
2026-03-08 23:56:31 +0300

    update boot.nix

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -4,9 +4,9 @@
   boot.kernelParams = [ "nowatchdog" ];
   boot.blacklistedKernelModules = [ "iTCO_wdt" "iTCO_vendor_support" ];
 
-  systemd.extraConfig = ''
-    DefaultTimeoutStopSec=10s
-    RuntimeWatchdogSec=0
-    ShutdownWatchdogSec=0
-  '';
+  systemd.settings.Manager = {
+      DefaultTimeoutStopSec = "10s";
+      RuntimeWatchdogSec = "0";
+      ShutdownWatchdogSec = "0";
+    };
 }
