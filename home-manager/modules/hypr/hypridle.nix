3c40a545ad2241a030246d1797efe5cba4a3a3b4
tatpow <popovvadimka2008@gmail.com>
2026-03-16 23:25:54 +0300

    commit

--- home-manager/modules/hypr/hypridle.nix
+++ home-manager/modules/hypr/hypridle.nix
@@ -11,26 +11,26 @@
 
       listener = [
         {
-          timeout = 20;
+          timeout = 150;
           on-timeout = "brightnessctl -s set 30";
           on-resume = "brightnessctl -r";
         }
         {
-          timeout = 20;
+          timeout = 150;
           on-timeout = "brightnessctl -sd asus::kbd_backlight set 0";
           on-resume = "brightnessctl -rd asus::kbd_backlight";
         }
         {
-          timeout = 30;
+          timeout = 300;
           on-timeout = "loginctl lock-session";
         }
         {
-          timeout = 40;
+          timeout = 600;
           on-timeout = "hyprctl dispatch dpms off";
           on-resume = "hyprctl dispatch dpms on";
         }
         {
-          timeout = 50;
+          timeout = 1200;
           on-timeout = "systemctl suspend";
         }
       ];
