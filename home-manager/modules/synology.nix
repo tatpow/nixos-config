e03fc9996a4ac024d1eec094e8d8dc6f92a6aac0
tatpow <popovvadimka2008@gmail.com>
2026-03-17 16:12:41 +0300

    commit

--- home-manager/modules/synology.nix
+++ home-manager/modules/synology.nix
@@ -1,8 +1,9 @@
 { pkgs, ... }:
 
 let
-  synology-fixed = pkgs.synology-drive.overrideAttrs (old: {
+  synology-fixed = pkgs.synology-drive-client.overrideAttrs (old: {
     nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ pkgs.makeWrapper ];
+    
     postInstall = (old.postInstall or "") + ''
       wrapProgram $out/bin/synology-drive \
         --set QT_QPA_PLATFORM xcb
